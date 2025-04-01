f3dc35545e27a1c35657bf79633b1ea658066762
svetnoch <aweet@mail.ru>
2025-04-01 15:24:31 +0300

    First commit

---
+++ src/kykyxa/Lord.java
@@ -0,0 +1,101 @@
+package kykyxa;
+
+import java.util.Scanner;
+
+public class Lord {
+    public static void main(String[] args) {
+
+//        int w = 0;
+//        while (w<=10){
+//            System.out.print(" Java" + " ");
+//            w++;
+//        }
+
+
+//        int i;
+//        for(i=0; i<=10; i++){
+//            System.out.print("Java ");
+//        }
+
+
+//        int i;
+//        for(i=0; i<=10; i++){
+//            System.out.println("Java ");
+//        }
+
+
+//        int year = 1980;
+//        do {
+//            System.out.println("Олимпиада " + year + " года");
+//            year = year + 4;
+//        }
+//        while (year <= 2020);
+
+
+//        for (int i = 1980; i <= 2020; i += 4) {
+//            System.out.println("Олимпиада " + i + " года");
+//        }
+
+
+//        int k = 3;
+//        for(int i =1; i<=9; i++){
+//            int w = k * i;
+//            System.out.println(i + " x " + k + " = " + w);
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите кол-во этажей в здании");
+//        int num = scanner.nextInt();
+//        if (num<=0){
+//            System.out.println("ошибка");
+//        }
+//        else if (num <=4) {
+//            System.out.print("Малоэтажный дом");
+//        }
+//        else if (num>5 && num<9){
+//            System.out.print("Среднеэтажный дом");
+//        }
+//        else {
+//            System.out.println("Многоэтажный дом");
+//
+//        }
+//        scanner.close(); // Закрываем Scanner для освобождения ресурсов
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//
+//        if (a >= b) {
+//            System.out.println("error");
+//        } else {
+//            for (int i = a + 1; i < b; i++) {
+//                if (i % 5 == 0 && i % 10 != 0) {
+//                    System.out.println(i + " ");
+//                }
+//            }
+//        }
+//        scanner.close();
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        if (a >= b) {
+//            System.out.println("error");
+//        } else {
+//            System.out.println("...");
+//            int i = a + 1;
+//            while (i < b) {
+//
+//                if (i % 5 == 0 && i % 10 != 0) {
+//                    System.out.println(i + " ");
+//                }
+//                    i++;
+//                }
+//        }
+//        scanner.close();
+    }
+}
+
+
